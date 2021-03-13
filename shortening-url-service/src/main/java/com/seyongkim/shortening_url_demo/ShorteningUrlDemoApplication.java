ec2d368a0fc1cc762b1d643b269300da88f300fd
jeff-seyong <sedragon.kim@gmail.com>
2021-03-13 15:43:06 +0900

    Create Project

---
+++ shortening-url-service/src/main/java/com/seyongkim/shortening_url_demo/ShorteningUrlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.seyongkim.shortening_url_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorteningUrlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorteningUrlDemoApplication.class, args);
+    }
+
+}
---
+++ shortening-url-service/src/test/java/com/seyongkim/shortening_url_demo/ShorteningUrlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyongkim.shortening_url_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorteningUrlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
