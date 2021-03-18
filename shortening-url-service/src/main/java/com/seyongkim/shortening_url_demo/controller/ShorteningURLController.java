a014e5baa0d1868fffa79f46cd59d13f701934f6
jeff-seyong <sedragon.kim@gmail.com>
2021-03-18 22:05:22 +0900

    add validation

--- shortening-url-service/src/main/java/com/seyongkim/shortening_url_demo/controller/ShorteningURLController.java
+++ shortening-url-service/src/main/java/com/seyongkim/shortening_url_demo/controller/ShorteningURLController.java
@@ -7,11 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class ShorteningURLController {
@@ -19,7 +23,7 @@ public class ShorteningURLController {
     private final ShorteningURLService shorteningURLService;
 
     @GetMapping(value = "/{shortenedUrl}")
-    public ResponseEntity<HttpStatus> getOriginalUrlAndRedirect(@PathVariable String shortenedUrl) throws NotFoundException {
+    public ResponseEntity<HttpStatus> getOriginalUrlAndRedirect(@PathVariable @NotBlank String shortenedUrl) throws NotFoundException {
         UrlDto.Info response = shorteningURLService.count(shortenedUrl);
 
         HttpHeaders headers = new HttpHeaders();
@@ -28,14 +32,14 @@ public class ShorteningURLController {
     }
 
     @GetMapping(value = "/url/count", params = {"shortenedUrl"})
-    public ResponseEntity<UrlDto.Info> getNumberOfUrlRequestByShortenedUrl(@RequestParam String shortenedUrl) throws NotFoundException {
+    public ResponseEntity<UrlDto.Info> getNumberOfUrlRequestByShortenedUrl(@RequestParam @NotBlank String shortenedUrl) throws NotFoundException {
         UrlDto.Info response = shorteningURLService.getNumberOfUrlRequestByShortenedUrl(shortenedUrl);
 
         return ResponseEntity.ok(response);
     }
 
     @GetMapping(value = "/url/count", params = {"originalUrl"})
-    public ResponseEntity<UrlDto.Info> getNumberOfUrlRequestByOriginalUrl(@RequestParam String originalUrl) throws NotFoundException {
+    public ResponseEntity<UrlDto.Info> getNumberOfUrlRequestByOriginalUrl(@RequestParam @NotBlank String originalUrl) throws NotFoundException {
         UrlDto.Info response = shorteningURLService.getNumberOfUrlRequestByOriginalUrl(originalUrl);
 
         return ResponseEntity.ok(response);
