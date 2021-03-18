3c2c002299ebb5585ebf54be215a74990d04b9b8
jeff-seyong <sedragon.kim@gmail.com>
2021-03-18 22:03:59 +0900

    add ShorteningUrlViewControllerTest

--- shortening-url-service/src/test/java/com/seyongkim/shortening_url_demo/controller/ShorteningUrlViewControllerTest.java
+++ shortening-url-service/src/test/java/com/seyongkim/shortening_url_demo/controller/ShorteningUrlViewControllerTest.java
@@ -3,44 +3,41 @@ package com.seyongkim.shortening_url_demo.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-// @SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ShorteningUrlViewControllerTest extends AbstractControllerTest {
 
     @Autowired
-    private ShorteningURLController shorteningURLController;
+    private ShorteningUrlViewController shorteningUrlViewController;
 
     @Override
     protected Object controller() {
-        return shorteningURLController;
+        return shorteningUrlViewController;
     }
 
-    // @Test
+    @Test
     void viewInputFormPageTest() throws Exception {
-        mockMvc.perform(get("/"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/").contentType(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
-    // @Test
+    @Test
     void createShorteningURLTest() throws Exception {
         MultiValueMap<String, String> requestParam = new LinkedMultiValueMap<>();
         requestParam.set("url", "https://www.musinsa.com");
         mockMvc.perform(MockMvcRequestBuilders.post("/")
                 .params(requestParam))
+                .andExpect(status().isOk())
                 .andReturn();
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(MockMvcResultMatchers.model().attribute("shortenedUrl", "Test"));
-
     }
 
 }
